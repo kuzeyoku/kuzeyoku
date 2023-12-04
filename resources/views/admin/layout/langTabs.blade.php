965d9d394f9031b064b93cb5cd6e72a8e144aff9
Ramazan Yüceloğlu <171201010@ogrenci.karatekin.edu.tr>
2023-12-04 22:17:12 +0300

    a

--- resources/views/admin/layout/langTabs.blade.php
+++ resources/views/admin/layout/langTabs.blade.php
@@ -1,8 +1,8 @@
 <ul class="nav mb-3">
     @foreach (languageList() as $key => $lang)
         <li class="nav-item">
-            <a class="nav-link rounded {{ $lang->code == app()->getLocale() ? 'active' : '' }}" data-bs-toggle="tab"
-                href="#{{ $lang->code }}" aria-selected="false" tabindex="-1">
+            <a class="nav-link rounded {{ $lang->code == config('app.fallback_locale') ? 'active' : '' }}"
+                data-bs-toggle="tab" href="#{{ $lang->code }}" aria-selected="false" tabindex="-1">
                 <span class="d-none d-sm-block">{{ $lang->title }}</span>
             </a>
         </li>
