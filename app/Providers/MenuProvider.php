8485b2e943ab44cd310d68b6f79bec4227ccc92d
Ramazan Yüceloğlu <171201010@ogrenci.karatekin.edu.tr>
2023-12-04 22:14:16 +0300

    s

--- app/Providers/MenuProvider.php
+++ app/Providers/MenuProvider.php
@@ -43,12 +43,10 @@ class MenuProvider extends ServiceProvider
         });
 
         view()->composer("layout.header", function ($view) use ($cache, $cacheTime) {
-            $headerMenu = $cache->remember("headerMenu", $cacheTime, function () {
-                $menu = new Menu();
-                if (!Schema::hasTable($menu->getTable()))
-                    return [];
-                return $menu->whereType("header")->order()->get();
-            });
+            // $headerMenu = $cache->remember("headerMenu", $cacheTime, function () {
+            $menu = new Menu();
+            $headerMenu = Schema::hasTable($menu->getTable()) ? $menu->whereType("header")->order()->get() : [];
+            // });
             $view->with(compact("headerMenu"));
         });
 
