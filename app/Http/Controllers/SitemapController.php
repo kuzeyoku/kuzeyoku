640fbf7379618c36486b322c8f9412e06f4678a4
Ramazan Yüceloğlu <171201010@ogrenci.karatekin.edu.tr>
2023-12-22 22:05:50 +0300

    end

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\StatusEnum;
-use App\Models\Page;
 use App\Models\Blog;
-use App\Models\Service;
-use App\Models\Project;
-use App\Models\Category;
+use App\Models\Page;
 use App\Models\Product;
+use App\Models\Project;
+use App\Models\Service;
+use App\Models\Category;
+use App\Enums\StatusEnum;
+use App\Models\Education;
 
 class SitemapController extends Controller
 {
@@ -20,7 +21,8 @@ class SitemapController extends Controller
         $services = Service::whereStatus(StatusEnum::Active->value)->get();
         $projects = Project::whereStatus(StatusEnum::Active->value)->get();
         $products = Product::whereStatus(StatusEnum::Active->value)->get();
-        $view = view("sitemap", compact("pages", "posts", "categories", "services", "projects", "products"));
+        $educations = Education::whereStatus(StatusEnum::Active->value)->get();
+        $view = view("sitemap", compact("pages", "posts", "categories", "services", "projects", "products", "educations"));
         return response($view)->header("Content-Type", "text/xml");
     }
 }
--- public/SertifikaYonetim/includes/footer.php
+++
@@ -1,7 +0,0 @@
-﻿<footer class="footer-area section_gap">
-        <div class="container" align="center">
-
-    <h2 align="center" style="color:#fff"> Sertifika Sorgulama Sistemi</h2>
-    <span style="color:#fff" >&copy;<a href="https://fcairstudio.com/" target="_blank"> Fc Havacılık</a></span>
-        </div>
-    </footer>
